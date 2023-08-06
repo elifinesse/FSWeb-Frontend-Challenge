7bfdc58fd57b2f736a3c56486bac5370d1b89632
elifinesse <133097200+elifinesse@users.noreply.github.com>
2023-08-06 17:57:20 +0300

    components + static data + very little css

---
+++ web-sayfam/src/components/Header.js
@@ -0,0 +1,33 @@
+export default function Header() {
+  return (
+    <div className="welcome-section">
+      <header>
+        <nav>
+          <a href="">TÜRKÇE'YE GEÇ</a>
+          <p>DARK MODE</p>
+        </nav>
+        <div>
+          <h2>elif</h2>
+          <nav>
+            <a href="#">Skills</a>
+            <a href="#">Projects</a>
+            <a href="#">Contact</a>
+          </nav>
+        </div>
+      </header>
+      <div>
+        <div>
+          <h1>I am a Frontend Developer...</h1>
+          <h3>
+            ...who likes to craft solid and scalable frontend products with
+            great user experiences.
+          </h3>
+          <div>
+            <button>Github</button>
+            <button>Linkedin</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- web-sayfam/tailwind.config.js
+++ web-sayfam/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "electric-blue": "#493fd3",
+      },
+    },
   },
   plugins: [],
 };
