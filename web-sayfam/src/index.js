02e927a57f72d7dd167d4dc10d79693ef68ef2cb
elifinesse <133097200+elifinesse@users.noreply.github.com>
2023-08-11 18:43:02 +0300

    new component

--- web-sayfam/src/index.js
+++ web-sayfam/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./components/Header.css";
+import "./components/Hero.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
