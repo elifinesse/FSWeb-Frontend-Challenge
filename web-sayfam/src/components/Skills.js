a8ece2d8d60b24493f67bd33059627760aee7144
elifinesse <133097200+elifinesse@users.noreply.github.com>
2023-08-12 15:46:46 +0300

    skills done

--- web-sayfam/src/components/Skills.js
+++ web-sayfam/src/components/Skills.js
@@ -1,24 +1,27 @@
 function Skills() {
   const skills = ["JAVASCRIPT", "REACT", "REDUX", "NODE", "VS CODE", "FIGMA"];
   return (
-    <div className="skills-section">
-      <div className="container">
-        <div className="yarim-daire">
-          <div className="yd-ic"></div>
-        </div>
-        <div className="flex flex-col items-center">
-          <h2 className="skills-heading">Skills</h2>
-          <div className="flex gap-12 flex-wrap">
-            {skills.map((skill, idx) => (
-              <div className="skill">
-                <img src={`./images/skills-${idx + 1}.png`} alt={skill} />
-                <p>{skill}</p>
-              </div>
-            ))}
+    <>
+      <div className="skills-section">
+        <div className="container">
+          <div className="yarim-daire">
+            <div className="yd-ic"></div>
+          </div>
+          <div className="flex flex-col items-center">
+            <h2 className="skills-heading">Skills</h2>
+            <div className="flex gap-12 flex-wrap">
+              {skills.map((skill, idx) => (
+                <div className="skill">
+                  <img src={`./images/skills-${idx + 1}.png`} alt={skill} />
+                  <p>{skill}</p>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
+        <div className="grey-pill"></div>
       </div>
-    </div>
+    </>
   );
 }
 
