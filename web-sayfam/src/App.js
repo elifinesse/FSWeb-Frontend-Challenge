964fee29415d653fe3c2ba012d8fc2132eb9179b
elifinesse <133097200+elifinesse@users.noreply.github.com>
2023-08-13 12:24:42 +0300

    toggle dark mode + dark mode codes

--- web-sayfam/src/App.js
+++ web-sayfam/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Profile from "./components/Profile";
@@ -6,6 +7,41 @@ import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(() => {
+    const userPref = window.matchMedia("(prefers-color-scheme: dark)");
+    if (userPref.matches) {
+      document.querySelector("html").classList.add("dark");
+    }
+    return userPref.matches;
+  });
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    document.querySelector("html").classList.toggle("dark", !darkMode);
+  };
+
+  const readUserColorPreference = () => {
+    const handler = ({ matches }) => {
+      if (matches) {
+        console.log("change to dark mode!");
+        setDarkMode(true);
+        document.querySelector("html").classList.add("dark");
+      } else {
+        console.log("change to light mode!");
+        setDarkMode(false);
+        document.querySelector("html").classList.remove("dark");
+      }
+    };
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    mediaQuery.addEventListener("change", handler);
+
+    // Cleanup function
+    return () => mediaQuery.removeEventListener("change", handler);
+  };
+
+  useEffect(() => {
+    readUserColorPreference();
+  });
   return (
     <div className="App">
       <Hero />
