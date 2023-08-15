c716ba220db62b880bd9b1a4520cba8ce5c469a5
elifinesse <133097200+elifinesse@users.noreply.github.com>
2023-08-15 11:30:50 +0300

    eng data done

---
+++ web-sayfam/src/data.js
@@ -0,0 +1,77 @@
+export const content = {
+  eng: {
+    header: {
+      dm: "DARK MODE",
+      lm: "LIGHT MODE",
+      prefix: "",
+      lang: "TÜRKÇE",
+      suffix: "'YE GEÇ",
+    },
+    hero: {
+      greeting: "Hi!",
+      name: "I am Elif.",
+      h2: "I’m a full-stack developer. I can craft solid and scalable frontend products. Let’s meet!",
+      first_black: "Currently",
+      first_pink: "Freelancing",
+      second_black: "for",
+      second_pink: "UX, UI, & Web Design",
+      third_black: "Projects",
+      invite: "Invite me to join your team -",
+      email: "elysaglam@gmail.com",
+    },
+    skills: { title: "SKILLS" },
+    profile: {
+      title: "PROFILE",
+      info_title: "Basic Information",
+      infoLabel: [
+        "Date of Birth",
+        "Place of Residence",
+        "Undergrad Education",
+        "Master's",
+        "Preferred Role",
+      ],
+      infoData: [
+        "14.03.1994",
+        "Aydin/Turkey",
+        "Bogazici Uni. English 2017",
+        "Heidelberg Uni. Transcultural Studies 2021",
+        "Frontend, UI",
+      ],
+      about: "About me",
+      p1: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.",
+      p2: "Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!",
+    },
+    projects: {
+      title: "PROJECTS",
+      projects: [
+        {
+          title: "Random Jokes",
+          description:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.",
+          tags: ["react", "vercel", "axios", "router"],
+          github_text: "View on Github",
+          github: "#",
+          app_text: "Go to App",
+          app: "#",
+        },
+        {
+          title: "Are you bored",
+          description:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia minima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!",
+          tags: ["react", "redux", "vercel", "axios", "router"],
+          github_text: "View on Github",
+          github: "#",
+          app_text: "Go to App",
+          app: "#",
+        },
+      ],
+    },
+    footer: {
+      text_prev: "Lets",
+      highlight: "work together",
+      text_end: "on your next product.",
+      blog: "Personal Blog",
+    },
+  },
+  tr: {},
+};
